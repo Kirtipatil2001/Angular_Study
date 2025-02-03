b5638db6df3f92284876f974a49d5f6d2365e08b
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2025-02-03 18:27:49 +0530

    Use of Change detection and Zone pollution

---
+++ change -detection/src/app/messages/messages.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, signal, Signal} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessagesService {
+  private messages= signal<string[]>([])
+  allMessages = this.messages.asReadonly();
+
+  addMessage(message: string) {
+    this.messages.update(oldMessages => [...oldMessages ,message]);
+  }
+}
